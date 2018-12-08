58a18cfc2d5eefdd226cfae5968aa4d0a69b2677
Guestman360 <matthewguest.dev@gmail.com>
2018-12-08 15:51:42 -0800

    Added all necessary view models to project

---
+++ Star Wars Info/Star Wars Info/View Models/InfoResourceDetailViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  InfoResourceDetailViewModel.swift
+//  Star Wars Info
+//
+//  Created by Matthew Guest on 12/8/18.
+//  Copyright © 2018 Matthew Guest. All rights reserved.
+//
+
+import Foundation
---
+++ Star Wars Info/Star Wars Info/View Models/SettingsViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  SettingsViewModel.swift
+//  Star Wars Info
+//
+//  Created by Matthew Guest on 12/8/18.
+//  Copyright © 2018 Matthew Guest. All rights reserved.
+//
+
+import Foundation
